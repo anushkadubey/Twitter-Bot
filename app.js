6ed515328e34e3bac1ee3494a2696dd613818646
Anushka Dubey <anushkadubey@Anushkas-MacBook-Air.local>
2025-12-28 17:45:35 +0530

    feat: update twitter API packages

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-const twit = require('twit');
-const config = require('./config.js');
-const app = new twit(config);
-console.log("twitter bot started");
-function retweet(){
-// Now setting up the search parameters. Only using "q" parameter here out of various parameters.
-let params = {                                    
-    q: "#webdev",
-    count: 10,
-    result_type: "recent",
-    lang: "en",
-    min_retweets: 10
-  }
+require("dotenv").config({  path: `${__dirname}/.env` });
 
-app.get("search/tweets", params, (err, data, response)=>
-{
-    let tweets=data.statuses;
-    if(!err){
-    for(let dat of tweets){
-        let retweetId = dat.id_str;
-        let username = dat.user.screen_name;
-        app.post("statuses/retweet/:id", {id : retweetId}, (err,response)=>{
-            if(response){
-            console.log("https://twitter.com/" + username + "/status/"+ retweetId);
-            }
-            else if(err){
-                console.log(err.message);
-            }
-        })
-    }
-    }
-    });
-}
-setInterval(retweet, 5000);
+const { twitterClient } = require("./twitterAPIConfig");
+
+const tweet = async () => {
+  try {
+    await twitterClient.v2.tweet("Hi. I am a bot tweeting about good news! #100DaysOfGoodNews");
+  } catch (error) {
+    console.log("Error occurred while tweeting: ", error);
+  }
+};
+
+tweet();
